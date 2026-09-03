711951232660b9f769500c80b77558185c24b593
Azkandyta Davian <azkandytadavian@gmail.com>
2026-09-03 22:17:34 +0700

    Add files via upload

---
+++ Tugas_Azka_Litera_Data.py
@@ -0,0 +1,32 @@
+data_string = "Azkandyta Davian Nusa Satrya"
+print("\nNama : ", data_string)
+data_integer = 18
+print("\nUmur : ", data_integer, "tahun")
+data_float = 56
+print("\nBerat : ", data_float, "Kg")
+
+data_str = "123"
+data_int = int(data_str)
+print("data = ", data_int, ",type = ", type(data_int))
+
+data_float = 45.67
+data_int = int(data_float)
+print("data = ", data_int, "type = ", type(data_int))
+
+data_int = 89
+data_float = float(data_int)
+print("data = ", data_float, "type = ", type(data_float))
+
+data_int = 89
+data_str = str(data_int)
+print("data = ", data_str, "type = ", type(data_str))
+
+usia = int(input("\nMasukkan usia: "))
+print("data = ", usia, "tahun", ",type = ", type(usia))
+
+
+tinggi_badan = float(input("\nMasukkan tinggi badan: "))
+print("data = ", tinggi_badan, "cm", ",type = ", type(tinggi_badan))
+
+nama = str(input("\nMasukkan nama: "))
+print("data = ", nama, ",type = ", type(nama))
